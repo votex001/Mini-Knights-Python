0369ad399b365dcff06848723315b00192e48aeb
votex001 <jeneea45@gmail.com>
2025-08-03 19:33:12 +0300

    smooth camera

--- camera.py
+++ camera.py
@@ -6,17 +6,46 @@ class Camera:
         self.player_rect = player_rect
         self.screen = screen
         self.zoom = zoom
-
+        self.camera_pos = [0,0]
+       
 
 
     def prepare_scaled_surface(self):
+        # zoom map
         scaled_surface = pygame.transform.scale(
             self.camera_surface, 
             (self.camera_surface.get_width() * self.zoom, self.camera_surface.get_height() * self.zoom))
-         
+        
 
-        surface_x = min(0,max((-self.player_rect.x * self.zoom) + self.screen.get_width() / 2,  -self.screen.get_width()*self.zoom +self.screen.get_width()))
-        surface_y = min(0,max((-self.player_rect.y*self.zoom)+self.screen.get_height()/2,-self.screen.get_height()*self.zoom + self.screen.get_height()))
-        surface_pos = (surface_x,surface_y)
-        return scaled_surface,surface_pos
+        player_pos_x = (-self.player_rect.x * self.zoom) + self.screen.get_width() / 2
+        player_pos_y = (-self.player_rect.y*self.zoom)+self.screen.get_height()/2
+
+        # dif between cam and player
+        dif_x = player_pos_x - self.camera_pos[0]
+        dif_y = player_pos_y - self.camera_pos[1]
+
+
+        # after this num camera pos = player pos
+        min_px = 1.0
+       
+        # to makes move camera smooth
+        smooth_factor = 0.1
+
+
+
+        # x
+        if abs(dif_x) < min_px:
+            self.camera_pos[0] = player_pos_x
+        else:
+            self.camera_pos[0] += dif_x * smooth_factor
+
+        # y
+        if abs(dif_y) < min_px:
+            self.camera_pos[1] = player_pos_y
+        else:
+            self.camera_pos[1] += dif_y * smooth_factor         
+
+        self.camera_pos[0] = min(0,max(self.camera_pos[0],  -self.screen.get_width()*self.zoom +self.screen.get_width()))
+        self.camera_pos[1] = min(0,max(self.camera_pos[1],-self.screen.get_height()*self.zoom + self.screen.get_height()))
+        return scaled_surface,self.camera_pos
     
