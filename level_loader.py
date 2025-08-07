a77830b42accae3d18b15c216fa4f194493284f6
votex001 <jeneea45@gmail.com>
2025-08-07 22:56:46 +0300

    optimise draw level

--- level_loader.py
+++ level_loader.py
@@ -11,6 +11,7 @@ class SelectLevel:
         self.tile_width = self.tmx_data.tilewidth
         self.tile_height = self.tmx_data.tileheight
         self.spawn_pos = None
+        self.sprite_group = None
     
 
     def get_spawn(self):
@@ -24,19 +25,30 @@ class SelectLevel:
                           
             
     def draw(self):
-        # draw bg
-        for layer in self.tmx_data.visible_layers:
-            if isinstance(layer,pytmx.TiledImageLayer):
-                image = layer.image
-                self.surface.blit(image,(0,0))
+        if not self.sprite_group:
+            self.sprite_group = pygame.sprite.Group()
+            # draw bg
+            for layer in self.tmx_data.visible_layers:
+                if isinstance(layer, pytmx.TiledImageLayer):
+                    image = layer.image
+                    image_sprite = pygame.sprite.Sprite()
+                    image_sprite.image = image
+                    image_sprite.rect = image.get_rect(topleft=(0, 0))
+                    self.sprite_group.add(image_sprite)
 
-        # draw tiles
-        for layer in self.tmx_data.visible_layers:
-            if isinstance(layer,pytmx.TiledTileLayer):
-                for x,y,gid in layer:
-                    tile = self.tmx_data.get_tile_image_by_gid(gid)
-                    if tile:
-                        self.surface.blit(tile,(x*self.tile_width,y*self.tile_height))
+            # draw tiles
+            for layer in self.tmx_data.visible_layers:
+                if isinstance(layer, pytmx.TiledTileLayer):
+                    for x, y, gid in layer:
+                        tile_image = self.tmx_data.get_tile_image_by_gid(gid)
+                        if tile_image:
+                            tile_sprite = pygame.sprite.Sprite()
+                            tile_sprite.image = tile_image
+                            tile_sprite.rect = tile_image.get_rect(topleft=(x * self.tile_width, y * self.tile_height))
+                            self.sprite_group.add(tile_sprite)
+
+        # draw once group
+        self.sprite_group.draw(self.surface)
 
 
 
