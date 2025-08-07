d1836b43e1794afb5898cd25d747e11366915b2c
votex001 <jeneea45@gmail.com>
2025-08-07 21:43:03 +0300

    exit title draw

--- level_loader.py
+++ level_loader.py
@@ -1,5 +1,6 @@
 import pytmx
 from pytmx.util_pygame import load_pygame
+import pygame
 
 
 
@@ -19,7 +20,8 @@ class SelectLevel:
                 self.spawn_pos = (obj.x,obj.y)
                 # self.spawn_pos = (0,0)
         return self.spawn_pos
-               
+
+                          
             
     def draw(self):
         # draw bg
@@ -35,21 +37,19 @@ class SelectLevel:
                     tile = self.tmx_data.get_tile_image_by_gid(gid)
                     if tile:
                         self.surface.blit(tile,(x*self.tile_width,y*self.tile_height))
-    def get_exit(self):
-        for obj in self.tmx_data.objects:
-            if obj.properties.get("exit") == True:
-                return (obj.x,obj.y, obj.width, obj.height)
-        return None
-    
-    def get_lvl_deathzones(self):
-        deathzones = []
-        for layer in self.tmx_data.visible_layers:
-            if isinstance(layer, pytmx.TiledTileLayer):
-                for x, y, gid in layer:
-                    tile_props = self.tmx_data.get_tile_properties_by_gid(gid)
-                    if tile_props and tile_props.get("deathzone") == True:
-                        deathzones.append((x * self.tile_width, y * self.tile_height, self.tile_width, self.tile_height))
-        return deathzones
+
+
+
+    def draw_exit_title(self):
+          title_font = pygame.font.Font(None,18)
+          text_surface = title_font.render('To exit press "F"',True,(255,255,255))
+          for layer in self.tmx_data.visible_layers:
+              if isinstance(layer,pytmx.TiledTileLayer):
+                  for x,y,gid in layer:
+                      tile_properties = self.tmx_data.get_tile_properties_by_gid(gid)
+                      if tile_properties and tile_properties.get("exit") == True and tile_properties.get("top_center") == True:
+                        text_rect = text_surface.get_rect(center = (x*self.tile_width+self.tile_width/2,y*self.tile_height-20))
+                        self.surface.blit(text_surface,text_rect)
 
     
     
